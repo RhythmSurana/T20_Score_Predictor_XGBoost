b2b03e691d155f10000e4036ddcd5465a97da0bf
Rhythm Surana <rhyts.surana@gmail.com>
2026-03-04 16:00:01 +0530

    Implement score prediction feature in Streamlit app
    Updated the Streamlit app to include score prediction logic based on user input.

--- main.py
+++ main.py
@@ -47,11 +47,19 @@ col3, col4, col5 = st.columns(3)
 with col3:
     current_score = st.number_input('Current Score')
 with col4:
-    overs_done = st.number_input('Overs Done (Works for >5)')
+    overs = st.number_input('Overs Done (Works for >5)')
 with col5:
     wickets = st.number_input('Wickets')
 
 last_five = st.number_input('Runs scored in Last Five Overs')
 
 if st.button('Predict Score'):
-    pass
+ balls_left = 120 - (overs * 6)
+ wickets_left = 10 - wickets
+ crr = current_score / overs
+
+ input_df = pd.DataFrame(
+  {'batting_team': [batting_team], 'bowling_team': [bowling_team], 'city': city, 'current_score': [current_score],
+   'balls_left': [balls_left], 'wickets_left': [wickets], 'crr': [crr], 'last_five': [last_five], 'last_30_runs': [last_five] })
+ result = pipe.predict(input_df)
+ st.header("Predicted Score - " + str(int(result[0])))
