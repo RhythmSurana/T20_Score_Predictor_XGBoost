74ea3eb9e8e77d9ac0a11fde02e19182923e498f
Rhythm Surana <rhyts.surana@gmail.com>
2026-03-04 15:33:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+pipe = pickle.load(open('pipe.pkl','rb'))
+
+teams = ['Tanzania',
+ 'Samoa',
+ 'Singapore',
+ 'Nigeria',
+ 'Saudi Arabia',
+ 'Pakistan',
+ 'Malaysia',
+ 'Finland',
+ 'Hong Kong',
+ 'Rwanda',
+ 'Romania',
+ 'Sri Lanka',
+ 'Germany',
+ 'Zimbabwe',
+ 'Malawi',
+ 'Qatar',
+ 'Estonia',
+ 'Kenya',
+ 'Uganda',
+ 'Bangladesh',
+ 'Austria',
+ 'Bahrain',
+ 'United Arab Emirates']
+
+cities = ['Kigali City', 'Singapore', 'Blantyre', 'Entebbe']
+
+st.title("Cricket Score Predictor")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    batting_team = st.selectbox('Select batting team', sorted(teams))
+with col2:
+    bowling_team = st.selectbox('Select bowling team', sorted(teams))
+
+city = st.selectbox('Select city', sorted(cities))
+
+col3, col4, col5 = st.columns(3)
+
+with col3:
+    current_score = st.number_input('Current Score')
+with col4:
+    overs_done = st.number_input('Overs Done (Works for >5)')
+with col5:
+    wickets = st.number_input('Wickets')
+
+last_five = st.number_input('Runs scored in Last Five Overs')
+
+if st.button('Predict Score'):
+    pass
